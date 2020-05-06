4fb67f3827534c8b6f2451dc3197907854afbd47
Florian Fröhlich <info@lumoc.de>
2020-05-06 14:34:07 +0200

    1.0.17

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let Accessory;
+let Accessory, Service, Characteristic, UUIDGen;
 const request = require("request");
 const ItemFactory = require('./libs/ItemFactory.js');
 const Utility = require('./libs/Utility.js');
